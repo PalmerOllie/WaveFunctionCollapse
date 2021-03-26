1429bfbba6c249d08ddd2a48fb36f572d26488fe
OlliePalmer <ollie.palmer@hl.co.uk>
2021-03-26 11:02:35 +0000

    Added css and basic grid

--- app.js
+++ app.js
@@ -2,3 +2,21 @@ var message = "Hello, TypeScript!";
 var heading = document.createElement("h1");
 heading.textContent = message;
 document.body.appendChild(heading);
+var grid = document.createElement("div");
+grid.className = "grid-container";
+document.body.appendChild(grid);
+for (var i = 0; i < 100; i++) {
+    grid.appendChild(makeGridItem(makeImageTile()));
+}
+function makeGridItem(image) {
+    var gridItem = document.createElement("div");
+    gridItem.className = "grid-item";
+    gridItem.appendChild(image);
+    return gridItem;
+}
+function makeImageTile() {
+    var defaultImage = document.createElement("img");
+    defaultImage.src = "images/simpleTileset/Grass/tile_0028.png";
+    defaultImage.className = "tile";
+    return defaultImage;
+}
--- app.ts
+++ app.ts
@@ -4,3 +4,26 @@ let heading = document.createElement("h1");
 heading.textContent = message;
 
 document.body.appendChild(heading);
+
+let grid = document.createElement("div");
+grid.className = "grid-container";
+document.body.appendChild(grid);
+
+for (let i = 0; i < 100; i++) {
+  grid.appendChild(makeGridItem(makeImageTile(), `tile${i}`));
+}
+
+function makeGridItem(image: HTMLImageElement, id: string): HTMLDivElement {
+  let gridItem = document.createElement("div");
+  gridItem.className = "grid-item";
+  gridItem.id = id;
+  gridItem.appendChild(image);
+  return gridItem;
+}
+
+function makeImageTile() {
+  let defaultImage = document.createElement("img");
+  defaultImage.src = "images/simpleTileset/Grass/tile_0028.png";
+  defaultImage.className = "tile";
+  return defaultImage;
+}
