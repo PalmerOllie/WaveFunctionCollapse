a34fbfaa5d9d5a2115b5a0de1d94f9819bbf8f7b
OlliePalmer <ollie.palmer@hl.co.uk>
2021-04-30 13:18:47 +0100

    Compiled dist

---
+++ dist/waveFuntionCollapse.js
@@ -0,0 +1,15 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+//Load prototype date
+//Initialise 2D/3D array
+//While not collapsed (no superpositions left)
+//Iterate
+//  - find lowest cell entropy
+//  - collapse at this cell
+//  - propogate changes
+//    - add changed cell to stack
+//    - check neighbours
+//    - update superpositions of these neighbours if needed
+//    - if did, add these cells to stack
+//    - keep going until stack empty
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoid2F2ZUZ1bnRpb25Db2xsYXBzZS5qcyIsInNvdXJjZVJvb3QiOiIiLCJzb3VyY2VzIjpbIi4uL3NyYy9zZXJ2ZXIvd2F2ZUZ1bnRpb25Db2xsYXBzZS50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOztBQUVBLHFCQUFxQjtBQUVyQix3QkFBd0I7QUFFeEIsOENBQThDO0FBRTlDLFNBQVM7QUFDVCw4QkFBOEI7QUFDOUIsMkJBQTJCO0FBQzNCLHVCQUF1QjtBQUN2QixpQ0FBaUM7QUFDakMsd0JBQXdCO0FBQ3hCLDJEQUEyRDtBQUMzRCx3Q0FBd0M7QUFDeEMsb0NBQW9DIn0=
